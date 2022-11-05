5bb2df2f4c39b565d905932f3c86341a1f8d2f93
Denys Smirnov <dendi239@gmail.com>
2022-11-05 16:03:57 +0200

    init commit

---
+++ carbon-podcasts/carbon-podcasts/carbon_podcastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  carbon_podcastsApp.swift
+//  carbon-podcasts
+//
+//  Created by Denys Smirnov on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct carbon_podcastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
